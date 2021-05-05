e48cc982b792687e6e0b6afb77cd35262b36d547
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-05-05 17:38:27 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
-console.log('hello')
-console.log('yeah')
+class BaseBallTeam {
+  name: string
+
+  constructor(name: string){
+      this.name = name
+  }
+
+  hello(){
+      console.log("チーム名は"+ this.name)
+  }
+
+}
+
+class Fielder{
+  name: string
+  ability: number
+
+  constructor(name:string, ability:number){
+      this.name = name
+      this.ability = ability
+  }
+}
+
+class Pitcher{
+  name: string
+  ability: number
+
+  constructor(name:string, ability:number){
+      this.name = name
+      this.ability = ability
+  }
+}
+
+console.log("-------------------")
+
+const ジョイアンツ = new BaseBallTeam("ジョイアンツ")
+ジョイアンツ.hello();
+
+const ティガース = new BaseBallTeam("ティガース")
+ティガース.hello();
