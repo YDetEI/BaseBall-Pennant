fb01b31e2895c417b5265304d761b2cc4303752f
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-05-11 23:52:23 +0900

    up

---
+++ src/lib/gameUtil.ts
@@ -0,0 +1,17 @@
+import { BaseBallTeam } from "../models/BaseBallTeam.model";
+
+type game ={
+   baseBallTeam1: BaseBallTeam
+   baseBallTeam2: BaseBallTeam
+
+   constructor(baseBallTeam1: BaseBallTeam, baseBallTeam2: BaseBallTeam){
+     this.baseBallTeam1 = BaseBallTeam
+     this.baseBallTeam2 = BaseBallTeam
+   }
+
+   judge(
+     
+   )
+}
+
+
