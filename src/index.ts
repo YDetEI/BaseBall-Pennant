ce6897199bbf3fa35139b96a5f8db457a44237f4
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-05-19 00:02:42 +0900

    up

--- src/index.ts
+++ src/index.ts
@@ -5,46 +5,7 @@ import { BASEBALL_TEAMS } from "./type"
 
 
 
-const list: number[] = [1, 2, 3]
-const books: string[] = ['教科書', '漫画']
-console.log(books)
-books.push('小説')
-console.log(books)
 
-// console.log(list)
-
-console.log("-------------------------------------------------------------------------------")
-
-
-class Human {
-  public gender:string
-  public JapaneseList: Japanese[] = []
-
-  constructor(gender: string, nameList: string[]){
-    this.gender = gender
-    nameList.forEach(name => {
-      const newJapanese = new Japanese(name)
-      this.JapaneseList.push(newJapanese)
-    })
-  }
-}
-
-class Japanese {
-  public name: string
-
-  constructor(name: string){
-    this.name = name
-  }
-
-  hello(){
-    console.log(`I am ${this.name}`)
-  }
-}
-
-const myFriends = ['こうちゃん', 'みっつ', 'MAX']
-const human = new Human('male', myFriends)
-console.log(human.JapaneseList)
-human.JapaneseList.forEach(japanese => japanese.hello())
 
 
 
@@ -79,12 +40,11 @@ const playerList: Player[] = [
   { name: 'だるびっしゅ',
     ability: 9999,
     playerType: 'Pitcher',
-    affiliation: 'Gs'
   },
+
   { name: 'じろう',
     ability: 99,
     playerType: 'Pitcher',
-    affiliation: 'Gs'
   }
 ]
 
--- src/models/BaseBallTeam.model.ts
+++ src/models/BaseBallTeam.model.ts
@@ -2,14 +2,32 @@ import { Player } from "./Player.model"
 
 export class BaseBallTeam {
   public name: string
-  public players: Player[] = []
+  public playerList: Player[] = []
   // カプセル化
 
+  /*
   constructor(name: string, players: string[]){
     this.name = name
     // this.players = players
     //BaseBallTeamをインスタンス化するときに、Playerも同時にインスタンス化する処理
     //PlayerたちをPlayer.model.tsにあるクラスをインスタンス化して配列なりなんなりでこのBaseballインスタンスのプロパティにいれる処理
   }
+*/
+
+constructor(name: string, playerList: string[])
+{
+  this.name = name
+
+  playerList.forEach(name => {
+    const newPlayer = new Player(name)
+    this.playerList.push(newPlayer)
+  },
+
+  
+  
+  )
+}
+
+console.log()
 
 }
