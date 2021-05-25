65523b5bc8b3225bbdc61ebd9e67c837cc0dc386
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-05-26 01:12:23 +0900

    up

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,12 @@
+import { Game } from "./lib/gameUtil"
 import { BaseBallTeam } from "./models/BaseBallTeam.model"
 import { Player } from "./models/Player.model"
+import { samplePlayers, sampleYokohama } from "./sampledata/samplePlayer"
 import { BASEBALL_TEAMS } from "./type"
 
+const Gs = new BaseBallTeam("Gs", samplePlayers)
+const Hanshin = new BaseBallTeam("Hanshin", samplePlayers)
+const ヤンキース = new BaseBallTeam("ヤンキース", samplePlayers)
+const Yokohamas = new BaseBallTeam("Yokohamas", sampleYokohama)
 
-
-
-
-
-
-
-
-
-
-// const baseballTeams: BaseBallTeam[] = []
-
-// BASEBALL_TEAMS.forEach(team => {
-//   baseballTeams.push(new BaseBallTeam(team))
-// })
-
-
-// baseballTeams.forEach(team => {
-//   team.hello()
-// })
-
-/*
-const gs = baseballTeams[0]
-gs.hello()
-
-const ts = baseballTeams[1]
-ts.hello()
-
-const Ichiro = new Player('イチロー', 4, 'Right')
-*/
-
-
-
-
-const playerList: Player[] = [
-  { name: 'だるびっしゅ',
-    ability: 9999,
-    playerType: 'Pitcher',
-  },
-
-  { name: 'じろう',
-    ability: 99,
-    playerType: 'Pitcher',
-  }
-]
-
-// const Gs = new BaseBallTeam('Gs', playerList)
-//Gs.hello()
-
-
-// const Darvish = new Player('ダルビッシュ', 9999, 'Pitcher', 'Gs')
-
+const GsVsHanshin = new Game(Gs, Hanshin)
--- src/models/Player.model.ts
+++ src/models/Player.model.ts
@@ -4,14 +4,14 @@ export class Player {
   public name: string
   public ability: number
   public playerType: PlayerType
-  
+
 
   constructor(name: string, ability: number, playerType: PlayerType){
     this.name = name
     this.ability = ability
     this.playerType = playerType
-    console.log(`I am ${this.name} and Ability is ${this.ability}
-    and PlayerType is ${this.playerType}. `)
+    // console.log(`I am ${this.name} and Ability is ${this.ability}
+    // and PlayerType is ${this.playerType}. `)
   }
 }
 
