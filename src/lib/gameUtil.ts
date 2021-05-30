b593a9554fb296bdf635fb2f307dee2b0d614f5f
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-05-30 19:42:40 +0900

    up

--- src/lib/gameUtil.ts
+++ src/lib/gameUtil.ts
@@ -1,4 +1,5 @@
 /*const get0ability = this.baseBallTeam1.playerList[0].ability*/
+/*const player = 配列.filter((player) => playerType !== 'Pitcher')*/
 import { BaseBallTeam } from '../models/BaseBallTeam.model'
 import { Player } from '../models/Player.model'
 
@@ -18,14 +19,44 @@ export class Game {
    ①野手９人のAbilityを抽出し、一回配列に入れる
    ②Pitcher5人からランダムで１人選び、そのAbilityを抽出し、①の配列に入れる
    */
-  public createArray4Judge(): number[] {
-    const getability1 = this.baseBallTeam1.playerList.map
-    if (this.baseBallTeam1.playerList[].playerType ===
-      'Catcher', 'First', 'Second', 'Third', 'Short', 'Left', 'Center', 'Right', 'DH')
-      return this.baseBallTeam1.playerList[].ability
-      else ;
-    }
-    /*
+  // public createArray4Judge(): number[] {
+  public createArray4Judge() {
+    const getAbility1 = this.baseBallTeam1.playerList
+      .filter((player) => player.playerType !== 'Pitcher')
+      .map((player) => player.ability)
+
+    console.log(getAbility1)
+  }
+
+  //if gusu return
+  //else not gusu continue
+  //console.log(gusu)
+  // const getAbility1 = this.baseBallTeam1.playerList.map((player: Player) => {
+  //   const currentPlayerType =
+  //   console.log(currentPlayerType)
+  //   // const isPitcher: boolean =
+  //   //if 投手でない場合、returnする。
+  //   // 投手でない場合はcontinueする。
+  // })
+
+  // if(条件式){
+  //   処理
+  // } else if (条件式) {
+  //   処理
+  // } else {
+  //   処理
+  // }
+  /*
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const gusu = numbers.filter((x) => x % 2 === 0)
+*/
+  /* if (this.baseBallTeam1.playerList[].playerType ===
+'Catcher', 'First', 'Second', 'Third', 'Short', 'Left', 'Center', 'Right', 'DH')
+return this.baseBallTeam1.playerList[].ability
+else ;
+}
+*/
+  /*
     const get0ability: number[] = [this.baseBallTeam1.playerList[0].ability]
     const get1ability: number[] = [this.baseBallTeam1.playerList[1].ability]
     const get2ability: number[] = [this.baseBallTeam1.playerList[2].ability]
@@ -44,7 +75,6 @@ export class Game {
 
 console.log(map1);
 */
-
 
   /*
    ③配列の中身をすべて足す
