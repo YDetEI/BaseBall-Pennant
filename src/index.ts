c629e00f1c62df24e8695bb1d234f057356c104e
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-06-06 13:33:00 +0900

    up

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import { Game } from './lib/gameUtil'
 import { BaseBallTeam } from './models/BaseBallTeam.model'
 import { Player } from './models/Player.model'
-import { sampleGs, sampleTs, sampleDs } from './sampledata/samplePlayer'
+import {
+  sampleGs,
+  sampleTs,
+  sampleDs,
+  sampleSs,
+  sampleBs,
+  sampleCs,
+  sampleFs,
+  sampleHs,
+  sampleEs,
+  sampleOs,
+  sampleLs,
+  sampleMs,
+} from './sampledata/samplePlayer'
 //import { AbilityResult, BASEBALL_TEAMS } from './type'
 
 const Gs = new BaseBallTeam('Gs', sampleGs)
 const Ts = new BaseBallTeam('Ts', sampleTs)
 const Ds = new BaseBallTeam('Ds', sampleDs)
+const Ss = new BaseBallTeam('Ss', sampleSs)
+const Bs = new BaseBallTeam('Bs', sampleBs)
+const Cs = new BaseBallTeam('Cs', sampleCs)
+const Fs = new BaseBallTeam('Fs', sampleFs)
+const Hs = new BaseBallTeam('Hs', sampleHs)
+const Es = new BaseBallTeam('Es', sampleEs)
+const Os = new BaseBallTeam('Os', sampleOs)
+const Ls = new BaseBallTeam('Ls', sampleLs)
+const Ms = new BaseBallTeam('Ms', sampleMs)
 
 const GsVsTs = new Game(Gs, Ts).judge()
 const GsVsDs = new Game(Gs, Ds).judge()
